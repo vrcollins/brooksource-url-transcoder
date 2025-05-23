29ae4955a9284cd29c38ee5dd1ad9d2873c012d4
Russell Collins <vrcollins@collinssolutions.org>
2025-05-23 12:41:37 -0500

    🔧 Adding Initial Configuration
    Adding Initial Spring Boot Initialization

---
+++ src/main/java/com/brooksource/urltranscoder/UrlTranscoderApplication.java
@@ -0,0 +1,13 @@
+package com.brooksource.url_transcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlTranscoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlTranscoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brooksource/urltranscoder/UrlTranscoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brooksource.url_transcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlTranscoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
